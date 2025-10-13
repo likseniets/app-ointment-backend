9d4a12b3aa97525d25e87b05c8e9c6e405191433
Sten-Eskil <Steineskil@gmail.com>
2025-10-13 11:17:35 +0200

    git ignore

--- obj/Debug/net8.0/app-ointment-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/app-ointment-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app-ointment-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e361be60d4af3b585df8e6bea532a76ee84a12")]
 [assembly: System.Reflection.AssemblyProductAttribute("app-ointment-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app-ointment-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
